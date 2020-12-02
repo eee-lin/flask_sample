ba3d79757c6c6b091d63d458affd8f5ce416c692
Yilin <yilinorwel1984@gmail.com>
2020-12-03 00:41:10 +0900

    for_task_in_tasks

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect #追加
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -17,9 +17,23 @@ class Todo(db.Model):
 @app.route('/', methods=['POST','GET'])
 def index():
     if request.method == 'POST':
-        return 'Hello'
+        # 新しいタスクを生成
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        # Pythonで例外（実行中に検出されたエラー）をキャッチして処理するにはtry, exceptを使う
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        # fail
+        except:
+            return 'There was an issue adding your task'
+
     else:
-        return render_template('index.html') 
+        # DBの全てをとってくる(日付順で)
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks) 
 
 if __name__ == "__main__":
     app.run(debug=True)
