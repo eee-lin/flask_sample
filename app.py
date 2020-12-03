e0c9e6a270362d2641a9308a6e0ea944a78a4026
Yilin <yilinorwel1984@gmail.com>
2020-12-03 18:16:30 +0900

    delete

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+#-*- coding: utf-8 -*-
+import sys
+sys.path.append("/Users/zhouyilin/Documents/Conv/flask")
 from flask import Flask, render_template, url_for, request, redirect #追加
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -35,5 +38,16 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks) 
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id) #idでタスクをとってくる
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+
+    except:
+        return 'There was a problem deleting your task'
 if __name__ == "__main__":
     app.run(debug=True)
